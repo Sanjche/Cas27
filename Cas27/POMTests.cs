eaee48715ea0e7e7114517f433d9c713d3c6794a
Skola Rajak QA <skolarajak.qa@gmail.com>
2021-05-31 22:23:48 +0200

    Started with Page Object Model

---
+++ Cas27/POMTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
+using Cas27.Lib;
+using Cas27.PageObjects;
+
+namespace Cas27
+{
+    class POMTests: BaseTest
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+            Logger.setFileName(@"C:\Kurs\POMTests.log");
+            this.driver = new ChromeDriver();
+            this.driver.Manage().Window.Maximize();
+            this.wait = new WebDriverWait(this.driver, new TimeSpan(0, 0, 10));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.driver.Close();
+        }
+
+        [Test]
+        [Category("POM")]
+        public void TestHomePage()
+        {
+            HomePage pomHomePage = new HomePage();
+            pomHomePage.Driver = this.driver;
+
+            pomHomePage.GoToPage();
+
+            pomHomePage.linkLogin.Click();
+
+            this.ExplicitWait(5000);
+        }
+
+    }
+}
---
+++ Cas27/PageObjects/HomePage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Cas27.Lib;
+
+namespace Cas27.PageObjects
+{
+    class HomePage: BaseTest
+    {
+
+        public HomePage()
+        {
+            Logger.setFileName(@"C:\Kurs\HomePage.log");
+        }
+
+        public IWebDriver Driver
+        {
+            set
+            {
+                this.driver = value;
+            }
+        }
+
+        public IWebElement linkLogin
+        {
+            get
+            {
+                return this.MyFindElement(By.LinkText("Login"));
+            }
+        }
+
+        public void GoToPage()
+        {
+            this.GoToURL("http://shop.qa.rs");
+        }
+    }
+}
